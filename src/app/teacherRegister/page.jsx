2f2cb530fecc13d82154026bd5d63d592abbf1a0
Nurspire <nurspire@gmail.com>
2025-06-01 18:01:02 +0500

    Do some fixes

--- src/app/teacherRegister/page.jsx
+++ src/app/teacherRegister/page.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import RegisterForm from '@/components/teacher/Registration/TeacherRegister'
-=======
-import RegisterForm from './RegisterForm'
->>>>>>> 80b69a8aef87a0fc8697f8b4f0e9b9241f3ac29e
 
 const page = () => {
   return (
